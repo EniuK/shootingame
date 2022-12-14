6ce7f44674e526f2c0aa354a8d01d782ae3f74bb
EniuK <108581363+EniuK@users.noreply.github.com>
2022-12-14 21:24:30 +0100

    checking stablity of the game

---
+++ game/src/App.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import "./App.css";
+
+type Enemy = {
+  top: number;
+  left: number;
+};
+
+function App() {
+  const [enemies, setEnemies] = useState<Enemy[]>([]);
+
+  function placeEnemy(e: React.MouseEvent<HTMLDivElement>) {
+    const { clientX, clientY } = e;
+    setEnemies([
+      ...enemies,
+      {
+        top: clientX,
+        left: clientY,
+      },
+    ]);
+  }
+  return (
+    <div className="App">
+      <div className="menu"></div>
+      <div className="game" onClick={placeEnemy}>
+        {enemies.map((e) => {
+          return (
+            <div className="enemy" style={{ top: e.top, left: e.left }}></div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
