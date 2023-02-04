3e593081e5f0b3750a0fa0b71a9915f25bd4fcc7
EniuK <108581363+EniuK@users.noreply.github.com>
2023-02-04 20:26:06 +0100

    something isnt working but im closer

--- game/src/App.tsx
+++ game/src/App.tsx
@@ -11,17 +11,33 @@ type Enemy = {
 const App = () => {
   const [enemies, setEnemies] = useState<Enemy[]>([]);
   const [time, setTime] = useState(1);
-  const [tick, setTick] = useState(true);
-  const placeEnemyButHelping = () => {
-    console.log("word");
+  const [tick, setTick] = useState(false);
+  const placeEnemyRndomlyGenerated = (x: number, y: number) => {
+    const spot = {
+      top: x,
+      left: y,
+      width: 50,
+      height: 50,
+    };
+    console.log(spot);
+    setEnemies([...enemies, spot]);
+    console.log("enemies");
   };
   const randomEnemyGenerator = () => {
-    const number = Math.floor(Math.random() * (100 - 1) + 1);
-    console.log(number);
-    number % 2 === 0 ? placeEnemyButHelping() : console.log("hihi");
+    const number1 = Math.floor(Math.random() * (100 - 1) + 1);
+    const numberX = Math.floor(Math.random() * (1400 - 1) + 1);
+    const numberY = Math.floor(Math.random() * (700 - 100) + 100);
+
+    // console.log(numberX);
+    // console.log(numberY);
+    number1 % 3 === 0
+      ? placeEnemyRndomlyGenerated(numberX, numberY)
+      : console.log("did not place enemy");
   };
   const placeEnemy = (event: MouseEvent<HTMLDivElement>) => {
     const { clientX, clientY } = event;
+    // console.log(clientX);
+    // console.log(clientY);
     randomEnemyGenerator();
     const client = {
       top: clientY,
@@ -32,13 +48,12 @@ const App = () => {
 
     setEnemies([...enemies, client]);
 
-    setTick(!tick);
+    // setTick(!tick);
   };
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setTime(time + 1);
-
+  const intervalFunction = () => {
+    setInterval(() => {
+      randomEnemyGenerator();
       const hehe = enemies.map((enemy) => {
         return {
           ...enemy,
@@ -47,13 +62,39 @@ const App = () => {
         };
       });
       setEnemies(hehe);
-    }, 1000);
-    return () => clearInterval(interval);
-  }, [tick, time]);
+      console.log(enemies);
+    }, 5000);
+  };
+  tick ? intervalFunction() : console.log("nope");
+
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setTime(time + 1);
+
+  //     const hehe = enemies.map((enemy) => {
+  //       return {
+  //         ...enemy,
+  //         height: enemy.height + 50,
+  //         width: enemy.width + 50,
+  //       };
+  //     });
+  //     setEnemies(hehe);
+  //   }, 1000);
+  //   return () => clearInterval(interval);
+  // }, [tick, time]);
   // didnt do anything :D
+
+  const helpMeeee = () => {
+    if (tick === true) {
+      return;
+    }
+    setTick(!tick);
+  };
   return (
     <div className="App">
-      <div className="menu" />
+      <div className="menu">
+        <button onClick={helpMeeee}>click to start</button>
+      </div>
       <div className="game" onClick={placeEnemy}>
         {enemies.map((enemy) => {
           const { top, left, width, height } = enemy;
